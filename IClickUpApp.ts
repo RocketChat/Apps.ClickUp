96542e571909ba871cfb1016852bd2a1c69d9747
Zamp <mustafahasankhan@icloud.com>
2022-06-14 03:07:31 -0700

    Added Extended Configuration for OAuth.

---
+++ IClickUpApp.ts
@@ -0,0 +1,7 @@
+export enum IMessageButonActions {
+    GoToSettings = 'goToSettings',
+    GoToTask = 'goToTask',
+    AuthClickUpOAuth = 'AuthClickUpOAuth',
+    RemoveNotifications = 'removeNotifications',
+    AddNotifications = 'addNotifications',
+}
