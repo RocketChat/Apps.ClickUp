6d91ca62132acb5be442cb4e19597befce7e8449
haemza30 <hamzam3032@gmail.com>
2023-04-05 13:11:33 +0530

    Fixed a minor issue

--- src/lib/delete/deleteFolder.ts
+++ src/lib/delete/deleteFolder.ts
@@ -4,23 +4,42 @@ import { UIKitBlockInteractionContext } from "@rocket.chat/apps-engine/definitio
 import { IUIKitBaseIncomingInteraction } from "@rocket.chat/apps-engine/definition/uikit/UIKitIncomingInteractionTypes";
 import { IUser } from "@rocket.chat/apps-engine/definition/users";
 import { getAccessTokenForUser } from "../../storage/users";
+import { HttpStatusCode } from '@rocket.chat/apps-engine/definition/accessors';
+import { getFolderUrl } from "../const";
 
-import { HttpStatusCode } from "@rocket.chat/apps-engine/definition/accessors";
-
-export async function deleteFolder({ context, data, room, read, persistence, modify, http }: { context: UIKitBlockInteractionContext; data: IUIKitBaseIncomingInteraction; room: IRoom; read: IRead; persistence: IPersistence; modify: IModify; http: IHttp }) {
-  const user: IUser = context.getInteractionData().user;
-  const token = await getAccessTokenForUser(read, user);
-  const folder_id = context.getInteractionData().value;
-  const headers = {
-    Authorization: `${token?.token}`,
-  };
-
-  const response = await http.del(`https://api.clickup.com/api/v2/folder/${folder_id}/`, { headers });
-  if (response.statusCode == HttpStatusCode.NO_CONTENT) {
-    const textSender = await modify.getCreator().startMessage().setText(`✅️ Folder deleted successfully!`);
-    if (room) {
-      textSender.setRoom(room);
-    }
+export async function deleteFolder({
+    context,
+    data,
+    room,
+    read,
+    persistence,
+    modify,
+    http,
+}: {
+    context: UIKitBlockInteractionContext,
+    data: IUIKitBaseIncomingInteraction,
+    room: IRoom;
+    read: IRead;
+    persistence: IPersistence;
+    modify: IModify;
+    http: IHttp;
+}) {
+    const user: IUser = context.getInteractionData().user;
+    const token = await getAccessTokenForUser(read, user);
+    const folder_id = context.getInteractionData().value;
+    const headers = {
+        Authorization: `${token?.token}`,
+    };
+    const url = getFolderUrl(folder_id!);
+    const response = await http.del(url, { headers });
+    if(response.statusCode==HttpStatusCode.NO_CONTENT) {
+        const textSender = await modify
+        .getCreator()
+        .startMessage()
+        .setText(`✅️ Folder deleted successfully!`);
+        if (room) {
+            textSender.setRoom(room);
+        }
     await modify.getCreator().finish(textSender);
   } else {
     const textSender = await modify.getCreator().startMessage().setText(`❗️ Unable to delete folder! \n Error ${response.data.err}`);
